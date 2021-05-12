0701913a9ee8e2884793958f57d05c385fd2885e
Macarena <macarenagonzalezalonso@gmail.com>
2021-05-12 10:32:42 +0200

    add js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,27 @@
-"use strict";
+'use strict';
 
+const randomNumber = getRandomNumber(100);
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
+
+console.log(`Mi número aleatorio es ${randomNumber}`);
+
+const numberElement = document.querySelector(".js-number");
+const buttonElement = document.querySelector(".js-button");
+const clueElement = document.querySelector(".js-clue");
+const tryElement = document.querySelector(".js-try");
+
+function getNumber() {
+  let numberValue = parseInt(numberElement.value);
+  console.log(numberElement.value);
+  if (numberValue === randomNumber) {
+    clueElement.value = 'Has ganado campeona!!!';
+  } else if (numberValue < randomNumber) {
+    clueElement.value = 'Demasiado bajo';
+  } else if (numberValue > randomNumber) {
+    clueElement.value = 'Demasiado alto';
+  }
+}
+
+buttonElement.addEventListener("click", getNumber);
