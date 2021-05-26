24d7e2fe896e4cdf07934235a95d2ff5c90411cc
Macarena <macarenagonzalezalonso@gmail.com>
2021-05-26 08:42:36 +0200

    completed

--- js/main.js
+++ js/main.js
@@ -1,29 +1,42 @@
-'use strict';
+"use strict";
 
 const randomNumber = getRandomNumber(100);
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
 
-console.log(`Mi número aleatorio es ${randomNumber}`);
-
 const numberElement = document.querySelector(".js-number");
 const buttonElement = document.querySelector(".js-button");
 const clueElement = document.querySelector(".js-clue");
 const tryElement = document.querySelector(".js-try");
+let cont = 0;
 
 function getNumber() {
   let numberValue = parseInt(numberElement.value);
   console.log(numberElement.value);
-  if (numberValue === randomNumber) {
-    clueElement.value = "Has ganado campeona!!!";
+  if (numberValue < 1 || numberValue > 100) {
+    clueElement.value = "El número debe estar entre 1 y 100";
   } else if (numberValue < randomNumber) {
-    clueElement.value = "Demasiado bajo";
+    clueElement.value = "Pista: Demasiado bajo";
   } else if (numberValue > randomNumber) {
-    clueElement.value = "Demasiado alto";
+    clueElement.value = "Pista: Demasiado alto";
+  } else if (numberValue === randomNumber) {
+    clueElement.value = "Has ganado campeona!!!";
   } else {
     clueElement.value = "El número debe estar entre 1 y 100";
   }
-};
+}
 
-buttonElement.addEventListener("click", getNumber);
+function increaseCounter() {
+  cont += 1;
+  tryElement.value = `Número de intentos: ${cont}`;
+}
+
+function handleClickButton() {
+  getNumber();
+  increaseCounter();
+}
+
+console.log("Random number: " + randomNumber);
+
+buttonElement.addEventListener("click", handleClickButton);
